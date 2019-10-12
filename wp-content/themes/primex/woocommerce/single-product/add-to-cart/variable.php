2cb866710fae177d57ca8c5155f14a91f30bbb73
Synthetica <i.synthetica@gmail.com>
2019-10-12 14:23:09 +0300

    Convert product page to wordpress

--- wp-content/themes/primex/woocommerce/single-product/add-to-cart/variable.php
+++ wp-content/themes/primex/woocommerce/single-product/add-to-cart/variable.php
@@ -42,6 +42,8 @@ do_action( 'woocommerce_before_add_to_cart_form' ); ?>
 									'options'   => $options,
 									'attribute' => $attribute_name,
 									'product'   => $product,
+                                    'class'     => 'form-control',
+                                    'id'        => 'select-' . $attribute_name
 								) );
 								echo end( $attribute_keys ) === $attribute_name ? wp_kses_post( apply_filters( 'woocommerce_reset_variations_link', '<a class="reset_variations" href="#">' . esc_html__( 'Clear', 'woocommerce' ) . '</a>' ) ) : '';
 							?>
