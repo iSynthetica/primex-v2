0e6bb3d162134b9c3cd28cbc7623cfb629bf91d6
Synthetica <i.synthetica@gmail.com>
2019-12-06 17:05:37 +0200

    Update wordpress and plugins

--- wp-content/plugins/wordpress-seo/admin/class-admin-asset-manager.php
+++ wp-content/plugins/wordpress-seo/admin/class-admin-asset-manager.php
@@ -330,10 +330,11 @@ class WPSEO_Admin_Asset_Manager {
 
 		return array(
 			array(
-				'name' => 'commons',
+				'name'      => 'commons',
 				// Load webpack-commons for bundle support.
-				'src'  => 'commons-' . $flat_version,
-				'deps' => array(
+				'src'       => 'commons-' . $flat_version,
+				'in_footer' => false,
+				'deps'      => array(
 					'wp-polyfill',
 				),
 			),
@@ -358,20 +359,10 @@ class WPSEO_Admin_Asset_Manager {
 				),
 			),
 			array(
-				'name' => 'help-center',
-				'src'  => 'wp-seo-help-center-' . $flat_version,
-				'deps' => array(
-					'jquery',
-					'wp-element',
-					'wp-i18n',
-					self::PREFIX . 'components',
-					self::PREFIX . 'commons',
-				),
-			),
-			array(
 				'name' => 'admin-script',
 				'src'  => 'wp-seo-admin-' . $flat_version,
 				'deps' => array(
+					'lodash',
 					'jquery',
 					'jquery-ui-core',
 					'jquery-ui-progressbar',
@@ -649,6 +640,16 @@ class WPSEO_Admin_Asset_Manager {
 					'wp-element',
 				),
 			),
+			array(
+				'name'      => 'help-scout-beacon',
+				'src'       => 'help-scout-beacon-' . $flat_version,
+				'in_footer' => false,
+				'deps'      => array(
+					self::PREFIX . 'styled-components',
+					'wp-element',
+					'wp-i18n',
+				),
+			),
 		);
 	}
 
@@ -693,6 +694,7 @@ class WPSEO_Admin_Asset_Manager {
 				'src'  => 'metabox-' . $flat_version,
 				'deps' => array(
 					self::PREFIX . 'select2',
+					self::PREFIX . 'admin-css',
 				),
 			),
 			array(
@@ -706,6 +708,9 @@ class WPSEO_Admin_Asset_Manager {
 			array(
 				'name' => 'adminbar',
 				'src'  => 'adminbar-' . $flat_version,
+				'deps' => array(
+					'admin-bar',
+				),
 			),
 			array(
 				'name' => 'primary-category',
