abadb2d3a666797a500218b1b394335730eb1971
Vlad Lisnyi <i.synthetica@gmail.com>
2019-12-08 13:52:26 +0200

    Show variations

--- wp-content/plugins/woo-all-in-one-catalogue/includes/functions.php
+++ wp-content/plugins/woo-all-in-one-catalogue/includes/functions.php
@@ -205,7 +205,17 @@ function wooaioc_get_product_categories_tree($parent = 0) {
                 $tree[$cat->term_id]['products'] = array();
 
                 foreach ($products as $product) {
+                    $product_type = $product->get_type();
                     $tree[$cat->term_id]['products'][$product->get_id()] = $product;
+                    if ('variable' === $product_type) {
+                        $available_variations = $product->get_available_variations();
+                        if (!empty($available_variations)) {
+                            foreach ($available_variations as $variation) {
+                                $variation_product = wc_get_product($variation['variation_id']);
+                                $tree[$cat->term_id]['products'][$variation_product->get_id()] = $variation_product;
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -225,29 +235,56 @@ function wooaioc_display_catalogue_item($item, $depth = 0) {
                 <?php
                 foreach ($item['products'] as $product) {
                     $product_data = $product->get_data();
+                    $product_type = $product->get_type();
                     ?>
                     <tr>
-                        <td>
-                            <div style="padding:1px 5px;">
-                                <?php
-                                if ( '' !== get_the_post_thumbnail($product_data['id']) ) {
-                                    ?>
-                                    <a href="<?php echo esc_url( get_permalink($product_data['id']) ); ?>">
-                                        <img class="image_fade" src="<?php echo get_the_post_thumbnail_url( $product_data['id'], 'thumbnail' ); ?>" alt="<?php echo $product_data['name'] ?>" style="max-height: 60px;width: auto;">
-                                    </a>
+                        <?php
+                        if ('variation' === $product_type) {
+                            ?>
+                            <td>
+                                <div style="padding:1px 5px 1px 3px;">
+                                    -
+                                </div>
+                            </td>
+                            <td>
+                                <div style="padding:1px 5px 1px 5px;">
                                     <?php
-                                }
-                                ?>
-                            </div>
-                        </td>
+                                    if ( '' !== get_the_post_thumbnail($product_data['id']) ) {
+                                        ?>
+                                        <a href="<?php echo esc_url( get_permalink($product_data['id']) ); ?>">
+                                            <img class="image_fade" src="<?php echo get_the_post_thumbnail_url( $product_data['id'], 'thumbnail' ); ?>" alt="<?php echo $product_data['name'] ?>" style="max-height: 45px;width: auto;">
+                                        </a>
+                                        <?php
+                                    }
+                                    ?>
+                                </div>
+                            </td>
+                            <?php
+                        } else {
+                            ?>
+                            <td colspan="2">
+                                <div style="padding:1px 5px 1px 0;">
+                                    <?php
+                                    if ( '' !== get_the_post_thumbnail($product_data['id']) ) {
+                                        ?>
+                                        <a href="<?php echo esc_url( get_permalink($product_data['id']) ); ?>">
+                                            <img class="image_fade" src="<?php echo get_the_post_thumbnail_url( $product_data['id'], 'thumbnail' ); ?>" alt="<?php echo $product_data['name'] ?>" style="max-height: 65px;width: auto;">
+                                        </a>
+                                        <?php
+                                    }
+                                    ?>
+                                </div>
+                            </td>
+                            <?php
+                        }
+                        ?>
                         <td>
                             <div style="padding:1px 5px;display: inline-block;vertical-align: middle;">
                                 <a href="<?php echo esc_url( get_permalink($product_data['id']) ); ?>">
                                     <?php echo $product_data['name'] ?>
                                 </a>
-                                <?php
-                                // var_dump($product_data);
-                                ?>
+                                <br>
+                                <?php echo $product_type; ?>
                             </div>
                         </td>
                         <td>
@@ -263,16 +300,28 @@ function wooaioc_display_catalogue_item($item, $depth = 0) {
                             </div>
                         </td>
                         <td>
-                            <div style="padding:1px 5px;display: inline-block;vertical-align: middle;">
-                                <input type="number" min="0" class="catalogue-item-qty" style="max-width:60px;">
-                            </div>
+                            <?php
+                            if ('variable' !== $product_type) {
+                                ?>
+                                <div style="padding:1px 5px;display: inline-block;vertical-align: middle;">
+                                    <input type="number" min="0" class="catalogue-item-qty" style="max-width:60px;">
+                                </div>
+                                <?php
+                            }
+                            ?>
                         </td>
                         <td>
-                            <div style="padding:1px 5px;display: inline-block;vertical-align: middle;">
-                                <button class="button" type="button">
-                                    <?php _e('Add to cart', 'woo-all-in-one-catalogue'); ?>
-                                </button>
-                            </div>
+                            <?php
+                            if ('variable' !== $product_type) {
+                                ?>
+                                <div style="padding:1px 5px;display: inline-block;vertical-align: middle;">
+                                    <button class="button" type="button">
+                                        <?php _e('Add to cart', 'woo-all-in-one-catalogue'); ?>
+                                    </button>
+                                </div>
+                                <?php
+                            }
+                            ?>
                         </td>
                     </tr>
                     <?php
