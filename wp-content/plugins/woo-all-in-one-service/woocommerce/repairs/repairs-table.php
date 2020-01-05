c85dcd0c3a159025d2095689630ac0c196138fde
Vlad Lisnyi <i.synthetica@gmail.com>
2020-01-05 22:57:15 +0200

    Added content functions

---
+++ wp-content/plugins/woo-all-in-one-service/woocommerce/repairs/repairs-table.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ *
+ */
+?>
+
+<div id="wooaioservice_list_container">
+    <?php
+    if (empty($repairs)) {
+        ?>
+
+        <?php
+    } else {
+        ?>
+        <table id="wooaioservice-table" class="wooaioservice-table shop_table table table-hover table-sm shop_table_responsive">
+            <?php
+            foreach ($repairs as $repair) {
+                ?>
+                <tr>
+                    <td>
+                        <?php echo $repair['title']; ?>
+                    </td>
+
+                    <td>
+                        <?php echo $repair['product']; ?>
+                    </td>
+
+                    <td>
+                        <?php echo $repair['fault']; ?>
+                    </td>
+
+                    <td>
+                        <?php
+                        echo "<pre>";
+                        print_r($repair);
+                        echo "</pre>";
+                        ?>
+                    </td>
+                </tr>
+                <?php
+            }
+            ?>
+        </table>
+        <?php
+    }
+    ?>
+</div>
